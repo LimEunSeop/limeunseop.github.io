c5496b4fc5a37ada8d25e0892eced2576162eac5
LimEunSeop <dmstjq12@naver.com>
2020-12-07 13:04:31 +0900

    블로그 스피너 추가 & flex layout 보완

--- src/containers/Blog/Cover.tsx
+++ src/containers/Blog/Cover.tsx
@@ -5,15 +5,33 @@ import styles from './Cover.module.scss'
 import withCoverAnimate from 'hoc/withCoverAnimate'
 import { parseString } from 'xml2js'
 import BlogPost, { BlogPostType } from 'components/BlogPost/BlogPost'
+import styled from '@emotion/styled'
+import { keyframes } from '@emotion/react'
+import { rem } from 'utils/styledComponentUtils'
 
 interface Props {
   coverColor: string
 }
 
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Spinner = styled.i`
+  display: inline-block;
+  width: ${rem(100)};
+  height: ${rem(100)};
+  font-size: ${rem(100)};
+  color: rgb(173, 181, 189);
+  animation: ${spin} 1.5s infinite linear;
+`
+
 const fetchUrl: string = `${process.env.NODE_ENV !== 'production' ? 'https://cors-anywhere.herokuapp.com/' : ''}https://medium.com/feed/@dmstjq92`
 
 const Cover = forwardRef<HTMLDivElement, Props>((props: Props, ref) => {
-  const [blogUrl, setBlogUrl] = useState<string | null>(null)
+  // const [blogUrl, setBlogUrl] = useState<string | null>(null)
   const [blogPosts, setBlogPosts] = useState<Array<BlogPostType> | null>(null)
 
   useEffect(() => {
@@ -40,7 +58,7 @@ const Cover = forwardRef<HTMLDivElement, Props>((props: Props, ref) => {
               post.link = post.link[0]
             })
           }
-          setBlogUrl(result?.rss?.channel[0]?.link as string)
+          // setBlogUrl(result?.rss?.channel[0]?.link as string)
           setBlogPosts(result?.rss?.channel[0]?.item?.slice(0, 4) as Array<BlogPostType>)
           console.log('Blog API result : ', result)
         })
@@ -58,13 +76,17 @@ const Cover = forwardRef<HTMLDivElement, Props>((props: Props, ref) => {
           </div>
         </div>
         <Heading.H className="a11yHidden">블로그</Heading.H>
-        <ul className={`resetList ${styles.blogPosts}`}>
-          {blogPosts?.map((post, i) => (
-            <li key={i}>
-              <BlogPost className={styles.post} data={post} />
-            </li>
-          ))}
-        </ul>
+        {blogPosts ? (
+          <ul className={`resetList ${styles.blogPosts}`}>
+            {blogPosts?.map((post, i) => (
+              <li key={i}>
+                <BlogPost className={styles.post} data={post} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <Spinner className={`fas fa-circle-notch ${styles.spinner}`} />
+        )}
       </div>
     </div>
   )
