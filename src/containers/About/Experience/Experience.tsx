c7f1297d4b4492bdc39fa17851936431ed63894b
LimEunSeop <dmstjq12@naver.com>
2021-11-08 16:13:22 +0900

    Update: About에 개인프로젝트 삭제

--- src/containers/About/Experience/Experience.tsx
+++ src/containers/About/Experience/Experience.tsx
@@ -2,7 +2,7 @@ import { Section, ListItem } from 'utils/makeResumeData'
 import { Heading } from '@tenon-io/tenon-ui'
 import styles from './Experience.module.scss'
 import JobExperience from './JobExperience'
-import PersonalProjects from './PersonalProjects'
+// import PersonalProjects from './PersonalProjects'
 
 interface Props {
   data: Section
@@ -24,7 +24,7 @@ export interface ProjectItem {
 
 const Experience = ({ data }: Props) => {
   const job_experience_section = data.children[0]
-  const personal_projects_section = data.children[1]
+  // const personal_projects_section = data.children[1]
 
   const job_experiences: Array<JobItem> = []
   job_experience_section.contents.forEach((content) => {
@@ -35,16 +35,16 @@ const Experience = ({ data }: Props) => {
     job_experiences.push({ title_markdown, date_string, description_markdown })
   })
 
-  const personal_projects: Array<ProjectItem> = []
-  personal_projects_section.contents.forEach((content) => {
-    const project_item = content as ListItem
-    const title_markdown: string = project_item.content
-    const date_string: string = project_item.children[0]
-    const description_markdown: string = project_item.children[1]
-    const lesson_markdown: string = project_item.children[2]
-    const skills_markdown: string = project_item.children[3]
-    personal_projects.push({ title_markdown, date_string, description_markdown, lesson_markdown, skills_markdown })
-  })
+  // const personal_projects: Array<ProjectItem> = []
+  // personal_projects_section.contents.forEach((content) => {
+  //   const project_item = content as ListItem
+  //   const title_markdown: string = project_item.content
+  //   const date_string: string = project_item.children[0]
+  //   const description_markdown: string = project_item.children[1]
+  //   const lesson_markdown: string = project_item.children[2]
+  //   const skills_markdown: string = project_item.children[3]
+  //   personal_projects.push({ title_markdown, date_string, description_markdown, lesson_markdown, skills_markdown })
+  // })
 
   return (
     <Heading.LevelBoundary>
@@ -52,7 +52,7 @@ const Experience = ({ data }: Props) => {
         <Heading.H className="a11yHidden">{data.title}</Heading.H>
         <Heading.LevelBoundary>
           <JobExperience data={job_experiences} />
-          <PersonalProjects data={personal_projects} />
+          {/* <PersonalProjects data={personal_projects} /> */}
         </Heading.LevelBoundary>
       </section>
     </Heading.LevelBoundary>
